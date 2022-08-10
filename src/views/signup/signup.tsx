df1548eca0bcd76fbe55504782e84ccb582643f9
Robin <carlosrobson0@gmail.com>
2022-08-10 17:40:19 +0200

    Validate signup and handle request error

--- src/views/signup/signup.tsx
+++ src/views/signup/signup.tsx
@@ -1,3 +1,4 @@
+import { InputHTMLAttributes, useState } from "react";
 import { Field, reduxForm } from "redux-form";
 import axios from "axios";
 import Button from "../../components/button/button";
@@ -5,23 +6,58 @@ import Header from "../../components/header/header";
 
 import styles from "./signup.module.css";
 
-interface SignupProps {}
+interface SignupProps {
+  handleSubmit: Function;
+}
 
-function Signup() {
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & {
-      name: { value: string };
-      surname: { value: string };
-      email: { value: string };
-      password: { value: string };
-    };
-    axios.post("https://frontend-test.getsandbox.com/users", {
-      username: target.name.value,
-      surname: target.surname.value,
-      email: target.email.value,
-      password: target.password.value,
-    });
+interface StringByString {
+  [key: string]: string;
+}
+
+interface FormValues extends StringByString {
+  username: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+interface RenderField {
+  input: InputHTMLAttributes<HTMLInputElement>;
+  label: string;
+  type: string;
+  meta: {
+    touched: boolean;
+    error: string;
+    warning: string;
+  };
+}
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error },
+}: RenderField) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+);
+
+function Signup(props: SignupProps) {
+  const { handleSubmit } = props;
+  const [requestError, setRequestError] = useState();
+
+  const submitForm = (data: FormValues) => {
+    console.log(data);
+    axios
+      .post("https://frontend-test.getsandbox.com/users", data)
+      .catch((error) => {
+        setRequestError(error.response?.data?.error?.message);
+      });
   };
 
   return (
@@ -29,35 +65,63 @@ function Signup() {
       <Header text="Signup Page" />
 
       <div className={styles.container}>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div className={styles.field}>
-            <label htmlFor="name">First Name </label>
-            <Field name="name" component="input" type="text" />
-          </div>
+        <form className={styles.form} onSubmit={handleSubmit(submitForm)}>
+          <Field
+            name="username"
+            component={renderField}
+            type="text"
+            label="First Name"
+          />
 
-          <div className={styles.field}>
-            <label htmlFor="surname">Last Name </label>
-            <Field name="surname" component="input" type="text" />
-          </div>
+          <Field
+            name="surname"
+            component={renderField}
+            type="text"
+            label="Last Name"
+          />
 
-          <div className={styles.field}>
-            <label htmlFor="surname">Email </label>
-            <Field name="email" component="input" type="text" />
-          </div>
+          <Field
+            name="email"
+            component={renderField}
+            type="text"
+            label="Email"
+          />
 
-          <div className={styles.field}>
-            <label htmlFor="surname">Password </label>
-            <Field name="password" type="password" component="input" />
-          </div>
+          <Field
+            name="password"
+            component={renderField}
+            type="password"
+            label="Password"
+          />
 
           <Button type="submit" label="Submit" />
+          <span>{requestError}</span>
         </form>
       </div>
     </>
   );
 }
+
+const validate = (values: FormValues) => {
+  enum fieldNames {
+    "username",
+    "surname",
+    "email",
+    "password",
+  }
+
+  const errors: StringByString = {};
+
+  for (const field in fieldNames) {
+    if (!values[field]) {
+      errors[field] = "Required";
+    }
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: "signup",
+  validate,
 })(Signup);
-
-// export default Signup;
